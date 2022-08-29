f082610942dba4403db8a2ca203a56d14765c87a
sanjay <webautomationdevelper@gmail.com>
2022-08-29 17:08:59 +0530

    tested

--- admin/index.php
+++ admin/index.php
@@ -25,9 +25,12 @@ if(!empty($_SESSION['admin'])){
       if($retval==0){
         echo '<script>alert("E-mail ID already exists");</script>';
         $type = 'addnew';    
-
       }
-
+      if($retval==1){
+        echo '<script>alert("Record Aadded Successfully");</script>';
+        header('location:http://localhost/hscnew/admin/index.php?&user='.$type);  
+      }
+      
 
 
     }
